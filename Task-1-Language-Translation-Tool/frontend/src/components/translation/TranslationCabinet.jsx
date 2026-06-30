1551686f3863062e0e1edb9fc83b889a3df407f7
KaustubhiDutta <kaustubhidutta2006@gmail.com>
2026-06-30 23:00:47 +0530

    Fix API URL

--- Task-1-Language-Translation-Tool/frontend/src/components/translation/TranslationCabinet.jsx
+++ Task-1-Language-Translation-Tool/frontend/src/components/translation/TranslationCabinet.jsx
@@ -58,8 +58,11 @@
 
     const test = async () => {
 
-      const response = await fetch(
-        "http://localhost:5000/detect",
+      const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+const response = await fetch(
+  `${API_URL}/detect`,
         {
           method: "POST",
           headers: {
